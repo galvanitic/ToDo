f54ca0f44ec8c746b3ef640b2d6e9607a62c6d85
galvanitic <develop@rjgalvan.com>
2020-01-22 14:47:08 -0600

    Finished To-DO

--- src/App.js
+++ src/App.js
@@ -2,22 +2,39 @@ import React from 'react';
 import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 
+
+
 class App extends React.Component {
 
   state = {
-    isClicked: false
+    isClicked: false,
+    text: 'Huh?',
+    toDos: []
   }
-  toggle = () => {
+  onClickHandler = () => {
     this.setState({isClicked: !this.state.isClicked})
     console.log(`THE STATE IS ${this.state.isClicked}`);
+    let newList = this.state.toDos.concat(this.state.text);
+    this.setState({toDos: newList})
+    this.setState({text: ''})
+  }
+  changeText = (e) => {
+    this.setState({text: e.target.value})
   }
 
 
   render() {
     return(
-      <button onClick={this.toggle}>
-        Was button clicked? {this.props.onClicked}
-      </button>
+      <div>
+        <h1>{this.state.text}</h1>
+        <ul>
+          {this.state.toDos.map((item, i) => <li key={i}> {item} </li>)}
+        </ul>
+        <input onChange={this.changeText}></input>
+        <button onClick={this.onClickHandler}>
+          Add to List
+        </button>
+      </div>
     )
   }
 
